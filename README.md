6e63cbbec7f6e0541b07c579410f0eaa401b2e6c
Marco Di Paola <dpl.marco@gmail.com>
2016-02-02 19:40:03 +0000

    lambda_poc

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # lambda_poc
-example lambda architecture using Kafka, Spark, Cassandra, Hadoop
+Poc for a lambda architecture using Kafka, Flume, Spark, Cassandra, HDFS
+
+#Prerequisites:
+* Java jdk 1.8.0_40
+* Scala 2.11
+* Sbt 0.13
+* Hadoop/HDFS 2.7.1
+* Spark 1.5.2
+* Cassandra 3.2.1
+* Kafka 0.9.0.0
+* Flume 1.6.0
+
+#Flume Config
+Create a conf file "lambdapoc_flume-conf.properties" and save it under the `conf` directory in the flume home
+
+    tier1.sources  = source1
+    tier1.channels = channel1
+    tier1.sinks = sink1
+    
+    tier1.sources.source1.type = org.apache.flume.source.kafka.KafkaSource
+    tier1.sources.source1.zookeeperConnect = localhost:2181
+    tier1.sources.source1.topic = events_topic
+    tier1.sources.source1.groupId = flume
+    tier1.sources.source1.channels = channel1
+    tier1.sources.source1.interceptors = i1
+    tier1.sources.source1.interceptors.i1.type = timestamp
+    tier1.sources.source1.kafka.consumer.timeout.ms = 100
+    
+    tier1.channels.channel1.type = memory
+    tier1.channels.channel1.capacity = 10000
+    tier1.channels.channel1.transactionCapacity = 1000
+    
+    tier1.sinks.sink1.type = hdfs
+    tier1.sinks.sink1.hdfs.path = hdfs://localhost:9000/new_data/kafka/%{topic}/%y-%m-%d
+    tier1.sinks.sink1.hdfs.rollInterval = 5
+    tier1.sinks.sink1.hdfs.rollSize = 0
+    tier1.sinks.sink1.hdfs.rollCount = 0
+    tier1.sinks.sink1.hdfs.fileType = DataStream
+    tier1.sinks.sink1.channel = channel1
+
+#Run the example
+Start HDFS:
+    
+    cd $HADOOP_HOME
+    sbin/start-all.sh
+
+
+Kafka comes with an embedded Zookeeper for testing purpose, you can start it with:
+
+    cd $KAFKA_HOME
+    bin/zookeeper-server-start.sh config/zookeeper.properties
+
+Start Kafka broker:
+
+    cd $KAFKA_HOME
+    bin/kafka-server-start.sh config/server.properties
+
+Create the topic 
+    
+    cd $KAFKA_HOME
+    bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic events_topic
+
+Start Flume agent:
+
+    cd $FLUME_HOME
+    bin/flume-ng agent -n tier1 -c conf -f conf/lambdapoc_flume-conf.properties
+
+You can send messages to Kafka via shell with:
+    
+    cd $KAFKA_HOME
+    bin/kafka-console-producer.sh --broker-list localhost:9092 --topic events_topic
+
+    {"event":"AAA", "timestamp":"2015-06-10 12:54:43"}
+    {"event":"AAA", "timestamp":"2015-06-10 12:54:43"}
+    {"event":"AAA", "timestamp":"2015-06-10 14:54:43"} 
+    {"event":"ZZZ", "timestamp":"2015-06-25 12:54:43"}
+    {"event":"ZZZ", "timestamp":"2015-06-25 12:54:53"}
+    ...
+    
+Start Cassandra:
+
+    cd $CASSANDRA_HOME
+    bin/cassandra -f
+    
+Start the speed layer / Spark Streaming by launching the `speed_layer.StreamingEventCounter` class
+
+
+Start the batch layer by launching the `batch_layer.BatchPipeline` class
+You can also launch the `batch_layer.DataPreProcessing` and `batch_layer.DataPreProcessor` separately if you want
+
+
+If you want to produce some test messages you can use the class `test.DataProducer`
--- src/main/scala/speed_layer/StreamingEventCounter2.scala
+++ src/main/scala/speed_layer/StreamingEventCounter2.scala
@@ -1,107 +1,76 @@
 package speed_layer
 
-import batch_layer.DateUtils._
 import com.datastax.driver.core.Cluster
+import com.datastax.spark.connector.cql.CassandraConnector
 import kafka.serializer.StringDecoder
+import model.Event
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{Column, DataFrame, SQLContext, SaveMode}
-import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.sql.cassandra.CassandraSQLContext
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 
-//ADD TIME MEASURE - METRICS
+object StreamingEventCounter2 extends App {
+  Logger.getRootLogger.setLevel(Level.WARN)
 
-object StreamingEventCounter2 {
-  val CASSANDRA_HOST = "127.0.0.1"
-  val KAFKA_BROKER_LIST = "localhost:9092"
+  val conf = new SparkConf()
+    .setAppName("StreamingEventCounter")
+    .setMaster("local[*]")
+    .set("spark.cassandra.connection.host", "127.0.0.1")
 
-  def main(args: Array[String]) {
-    Logger.getRootLogger.setLevel(Level.WARN)
+  val ssc = new StreamingContext(conf, Seconds(4))
 
-    prepareDatabase(CASSANDRA_HOST)
+  val cc = new CassandraSQLContext(ssc.sparkContext)
+  cc.setKeyspace("lambda_poc")
 
-    val conf = new SparkConf().setAppName("StreamingEventCounter").setMaster("local[*]")
-      .set("spark.sql.shuffle.partitions", "1")
-      .set("spark.cassandra.connection.host", CASSANDRA_HOST)
+  val connector = CassandraConnector(conf)
 
-    val ssc = new StreamingContext(conf, Seconds(1))
-    val sqlContext = new SQLContext(ssc.sparkContext)
+  val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
+  val session = cluster.connect()
+  session.execute("CREATE KEYSPACE IF NOT EXISTS lambda_poc WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };")
+  session.execute("CREATE TABLE IF NOT EXISTS lambda_poc.events (event text, bucket text, bdate timestamp, count counter, PRIMARY KEY ((event, bucket), bdate) );")
+  session.execute("TRUNCATE lambda_poc.events;")
 
-    val stream = getStream(ssc)
+  val topics = Set("events_topic")
+  val kafkaParams = Map[String, String](
+    "metadata.broker.list" -> "localhost:9092",
+    "group.id" -> "speed_layer"
+  )
 
-    stream.map { case (key, value) => value }.foreachRDD(rdd => {
-      if (!rdd.isEmpty()) {
-        val df = sqlContext.read.json(rdd).withColumn("count", lit(1))
+  val stream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
 
-        println("Aggregating by minute...")
+  val bucketedEvents = stream.flatMap {
+    jsonEvent =>
+      val e = Event.fromJson(jsonEvent._2)
+      e.buckets.map(b => (b, e.`type`))
+  }
 
-        val eventsPerMinute = df.groupBy(
-          col("event"),
-          year(col("timestamp")) as "year",
-          month(col("timestamp")) as "month",
-          dayofmonth(col("timestamp")) as "day",
-          hour(col("timestamp")) as "hour",
-          minute(col("timestamp")) as "minute"
-        ).count().cache()
+  val bucketedEventCounts = bucketedEvents
+    .groupByKey
+    .map { case (bucket, events) =>
+      val countsByEventType = events.groupBy(identity).mapValues(_.size)
+      (bucket, countsByEventType)
+    }
 
+  //TODO use batch updates!
 
-        saveToCassandra(eventsPerMinute, "m", bdate_min(col("year"), col("month"), col("day"), col("hour"), col("minute")))
+  bucketedEventCounts.foreachRDD { rdd =>
+    rdd.foreach { case (bucket, aggregates) =>
+      aggregates.foreach { case (eventType, count) =>
 
-        println("Aggregating by hour...")
+        println(s"${bucket.toString} - $eventType - ${count.toInt}")
 
-        val eventsPerHour = eventsPerMinute.groupBy(
-          col("event"),
-          col("year") as "year",
-          col("month") as "month",
-          col("day") as "day",
-          col("hour") as "hour"
-        ).count()
+        connector.withSessionDo { session =>
+          val prepared = session.prepare("UPDATE lambda_poc.events SET count = count + ? WHERE event = ? AND bucket = ? AND bdate = ? ")
+          session.execute(prepared.bind(new java.lang.Long(count), eventType, bucket.bucket, bucket.date.toDate))
+        }
 
-        saveToCassandra(eventsPerHour, "H", bdate_h(col("year"), col("month"), col("day"), col("hour")))
-
-        eventsPerMinute.unpersist()
-        /*
-          there shouldn't be the need for saving the D,M,Y buckets into the realtime views because data in RT views expires as
-          soon as the batch views absorb them (it should happen "often"")
-
-          saveToCassandra(df, "D", bdate_d(year(col("timestamp")), month(col("timestamp")), dayofmonth(col("timestamp"))))
-          saveToCassandra(df, "M", bdate_m(year(col("timestamp")), month(col("timestamp"))))
-          saveToCassandra(df, "Y", bdate_y(year(col("timestamp"))))
-        */
       }
-    })
+    }
 
-    ssc.start()
-    ssc.awaitTermination()
   }
 
-  def getStream(ssc: StreamingContext): InputDStream[(String, String)] = {
-    val kafkaParams = Map[String, String](
-      "metadata.broker.list" -> KAFKA_BROKER_LIST,
-      "group.id" -> "speed_layer"
-    )
-
-    KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, Set("events_topic"))
-  }
-
-  def saveToCassandra(df: DataFrame, bucket: String, bdate: Column) = {
-    println(s"Saving data with bucket [$bucket] : DF size => ${df.count()}")
-
-    df.withColumn("bucket", lit(bucket)).withColumn("bdate", bdate).
-      select("event", "bucket", "bdate", "count").
-      write.mode(SaveMode.Append).format("org.apache.spark.sql.cassandra")
-      .options(Map("table" -> "events", "keyspace" -> "lambda_poc"))
-      .save()
-  }
-
-  def prepareDatabase(cassandraHost: String) {
-    val cluster = Cluster.builder().addContactPoint(cassandraHost).build()
-    val session = cluster.connect()
-    session.execute("CREATE KEYSPACE IF NOT EXISTS lambda_poc WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };")
-    session.execute("CREATE TABLE IF NOT EXISTS lambda_poc.events (event text, bucket text, bdate timestamp, count counter, PRIMARY KEY ((event, bucket), bdate) );")
-    session.execute("TRUNCATE lambda_poc.events;")
-  }
+  ssc.start()
+  ssc.awaitTermination()
 }
